2e7fb2b050533bd276bb6b147f6fa6462a46796e
giacomoborsellino <giacomoborsellino@hotmail.it>
2023-08-17 17:32:25 +0200

    Updates

--- js/openCard.js
+++ js/openCard.js
@@ -1,151 +1,161 @@
-
 // TAB 1
 let tab1 = document.body.getElementsByClassName("tab1")[0];
 
 // FUNCTION OPEN CARD
 const readMore1 = () => {
-    tab1.style.display = "block"
-}
+  tab1.style.display = "block";
+};
 
 // FUNCTION CLOSE CARD
 const closeCard1 = () => {
-    tab1.classList.add("tabCardContainerClose");
+  tab1.classList.add("tabCardContainerClose");
 
-    setTimeout(() => {
-        tab1.classList.remove("tabCardContainerClose");
-        tab1.style.display = "none";
-    },499);
-}
-
+  setTimeout(() => {
+    tab1.classList.remove("tabCardContainerClose");
+    tab1.style.display = "none";
+  }, 499);
+};
 
 // TAB 2
 let tab2 = document.body.getElementsByClassName("tab2")[0];
 
 // FUNCTION OPEN CARD
 const readMore2 = () => {
-    tab2.style.display = "block"
-}
+  tab2.style.display = "block";
+};
 
 // FUNCTION CLOSE CARD
 const closeCard2 = () => {
-    tab2.classList.add("tabCardContainerClose");
+  tab2.classList.add("tabCardContainerClose");
 
-    setTimeout(() => {
-        tab2.classList.remove("tabCardContainerClose");
-        tab2.style.display = "none";
-    },499);
-}
-
+  setTimeout(() => {
+    tab2.classList.remove("tabCardContainerClose");
+    tab2.style.display = "none";
+  }, 499);
+};
 
 // TAB 3
 let tab3 = document.body.getElementsByClassName("tab3")[0];
 
 // FUNCTION OPEN CARD
 const readMore3 = () => {
-    tab3.style.display = "block"
-}
+  tab3.style.display = "block";
+};
 
 // FUNCTION CLOSE CARD
 const closeCard3 = () => {
-    tab3.classList.add("tabCardContainerClose");
+  tab3.classList.add("tabCardContainerClose");
 
-    setTimeout(() => {
-        tab3.classList.remove("tabCardContainerClose");
-        tab3.style.display = "none";
-    },499);
-}
-
+  setTimeout(() => {
+    tab3.classList.remove("tabCardContainerClose");
+    tab3.style.display = "none";
+  }, 499);
+};
 
 // TAB 4
 let tab4 = document.body.getElementsByClassName("tab4")[0];
 
 // FUNCTION OPEN CARD
 const readMore4 = () => {
-    tab4.style.display = "block"
-}
+  tab4.style.display = "block";
+};
 
 // FUNCTION CLOSE CARD
 const closeCard4 = () => {
-    tab4.classList.add("tabCardContainerClose");
+  tab4.classList.add("tabCardContainerClose");
 
-    setTimeout(() => {
-        tab4.classList.remove("tabCardContainerClose");
-        tab4.style.display = "none";
-    },499);
-}
-
+  setTimeout(() => {
+    tab4.classList.remove("tabCardContainerClose");
+    tab4.style.display = "none";
+  }, 499);
+};
 
 // TAB 5
 let tab5 = document.body.getElementsByClassName("tab5")[0];
 
 // FUNCTION OPEN CARD
 const readMore5 = () => {
-    tab5.style.display = "block"
-}
+  tab5.style.display = "block";
+};
 
 // FUNCTION CLOSE CARD
 const closeCard5 = () => {
-    tab5.classList.add("tabCardContainerClose");
+  tab5.classList.add("tabCardContainerClose");
 
-    setTimeout(() => {
-        tab5.classList.remove("tabCardContainerClose");
-        tab5.style.display = "none";
-    },499);
-}
-
+  setTimeout(() => {
+    tab5.classList.remove("tabCardContainerClose");
+    tab5.style.display = "none";
+  }, 499);
+};
 
 // TAB 6
 let tab6 = document.body.getElementsByClassName("tab6")[0];
 
 // FUNCTION OPEN CARD
 const readMore6 = () => {
-    tab6.style.display = "block"
-}
+  tab6.style.display = "block";
+};
 
 // FUNCTION CLOSE CARD
 const closeCard6 = () => {
-    tab6.classList.add("tabCardContainerClose");
+  tab6.classList.add("tabCardContainerClose");
 
-    setTimeout(() => {
-        tab6.classList.remove("tabCardContainerClose");
-        tab6.style.display = "none";
-    },499);
-}
-
+  setTimeout(() => {
+    tab6.classList.remove("tabCardContainerClose");
+    tab6.style.display = "none";
+  }, 499);
+};
 
 // TAB 7
 let tab7 = document.body.getElementsByClassName("tab7")[0];
 
 // FUNCTION OPEN CARD
 const readMore7 = () => {
-    tab7.style.display = "block"
-}
+  tab7.style.display = "block";
+};
 
 // FUNCTION CLOSE CARD
 const closeCard7 = () => {
-    tab7.classList.add("tabCardContainerClose");
+  tab7.classList.add("tabCardContainerClose");
 
-    setTimeout(() => {
-        tab7.classList.remove("tabCardContainerClose");
-        tab7.style.display = "none";
-    },499);
-}
-
+  setTimeout(() => {
+    tab7.classList.remove("tabCardContainerClose");
+    tab7.style.display = "none";
+  }, 499);
+};
 
 // TAB 8
 let tab8 = document.body.getElementsByClassName("tab8")[0];
 
 // FUNCTION OPEN CARD
 const readMore8 = () => {
-    tab8.style.display = "block"
-}
+  tab8.style.display = "block";
+};
 
 // FUNCTION CLOSE CARD
 const closeCard8 = () => {
-    tab8.classList.add("tabCardContainerClose");
+  tab8.classList.add("tabCardContainerClose");
 
-    setTimeout(() => {
-        tab8.classList.remove("tabCardContainerClose");
-        tab8.style.display = "none";
-    },499);
-}
+  setTimeout(() => {
+    tab8.classList.remove("tabCardContainerClose");
+    tab8.style.display = "none";
+  }, 499);
+};
+
+// TAB 9
+let tab9 = document.body.getElementsByClassName("tab9")[0];
+
+// FUNCTION OPEN CARD
+const readMore9 = () => {
+  tab9.style.display = "block";
+};
+
+// FUNCTION CLOSE CARD
+const closeCard9 = () => {
+  tab9.classList.add("tabCardContainerClose");
+
+  setTimeout(() => {
+    tab9.classList.remove("tabCardContainerClose");
+    tab9.style.display = "none";
+  }, 499);
+};
